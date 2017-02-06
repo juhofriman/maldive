2cc7bed9e81af4ec973f3ee3cf98160ad062c57c
Juho Friman <juho.friman@solita.fi>
2017-02-06 10:19:09 +0200

    Store is now easier to use. It just persists stuff.

--- src/maldive/generic_in_memory_store.clj
+++ src/maldive/generic_in_memory_store.clj
@@ -1,20 +1,27 @@
 (ns maldive.generic-in-memory-store)
 
-(def seqs {:generic-entity (atom 1)
-           :memo (atom 1)})
+(def seqs (atom {}))
 
-(def storage {:generic-entity (atom {})
-              :memo (atom {})})
+(def storage (atom {}))
+
+(defn get-storage
+  [type]
+  (if (contains? @storage type)
+    (get @storage type)
+    (do
+      (swap! seqs assoc type (atom 1))
+      (get (swap! storage assoc type (atom {})) type))))
 
 (defn store
   [type obj]
-  (let [store (get storage type) id (or (get obj "id") (swap! (:generic-entity seqs) inc))]
-    (swap! store assoc id (assoc obj "id" id))))
+  (let [store (get-storage type)
+        id (or (get obj "id") (swap! (get @seqs type) inc))]
+     (swap! store assoc id (assoc obj "id" id))))
 
 (defn docs-by-type
   [key]
-  (map second (into [] @(get storage key))))
+  (map second (into [] @(get-storage key))))
 
 (defn get-entity
   [type id]
-  (get @(get storage type) id))
+  (get @(get-storage type) id))
